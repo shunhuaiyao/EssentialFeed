c296438e3c2d281557630349a0e349fa089e3ea8
shunhuaiyao <shunhuai.yao@gmail.com>
2022-01-27 09:55:49 +0800

    Initial Commit

---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Sean Yao on 2022/1/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
