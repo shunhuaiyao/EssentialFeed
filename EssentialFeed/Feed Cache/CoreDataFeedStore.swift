b96bfcf98825a3f7a05e668d12b3fdcc70d5df5d
shunhuaiyao <shunhuai.yao@gmail.com>
2022-06-02 09:11:48 +0800

    make `storeURL` an explicit dependency so we can inject test-specific URLs (such as `/dev/null`) to avoid sharing state with production (and other tests!)

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -13,10 +13,10 @@ public class CoreDataFeedStore: FeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
-    public init(bundle: Bundle = .main) throws {
-        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
-        context = container.newBackgroundContext()
-    }
+    public init(storeURL: URL, bundle: Bundle = .main) throws {
+            container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
+            context = container.newBackgroundContext()
+        }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
@@ -50,12 +50,15 @@ private extension NSPersistentContainer {
         case failedToLoadPersistentStores(Error)
     }
 
-    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+    static func load(modelName name: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
         guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
             throw LoadingError.modelNotFound
         }
 
+        let description = NSPersistentStoreDescription(url: url)
         let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        container.persistentStoreDescriptions = [description]
+
         var loadError: Error?
         container.loadPersistentStores { loadError = $1 }
         try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
