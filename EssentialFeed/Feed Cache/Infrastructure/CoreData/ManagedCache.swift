389cf6c148d95d6374ad597137621b6727a86204
shunhuaiyao <shunhuai.yao@gmail.com>
2022-06-11 13:52:48 +0800

    separate CoreData managed model classes data from helpers with extensions

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -11,7 +11,9 @@ import CoreData
 internal class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
-    
+}
+
+extension ManagedCache {
     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         guard let entityName = entity().name else { return nil }
         let request = NSFetchRequest<ManagedCache>(entityName: entityName)
